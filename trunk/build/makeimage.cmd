a64825273ef33e6b0508465fb09b93bb469e82ed
killerxx7 <killerxx7@2448ddd4-6948-0410-b68c-cfbae435342e>
2008-03-17 20:33:08 +0000

    Упрощение сборки

--- trunk/build/makeimage.cmd
+++ trunk/build/makeimage.cmd
@@ -2,6 +2,7 @@
 echo.
 echo Creating floppy image
 if exist ~Debug\floppy.ima del ~Debug\floppy.ima
+if exist ~Temp\kernel.exe move ~Temp\kernel.exe ~Debug\kernel.exe
 cd ~Debug
 ..\tools\makefd.exe .
 cd ..
